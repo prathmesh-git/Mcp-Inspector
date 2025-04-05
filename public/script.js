9d39c62d568c951f0dcdf2ee376210bd9299c0f9
Prathmesh Pimpalshende <prathmpimpalshende@gmail.com>
2025-04-05 12:17:37 +0530

    connected frontend with backendd

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
         resultDiv.innerHTML = ""; 
 
         try {
-            const response = await fetch("http://localhost:5000/test-mcp", {
+            const response = await fetch("api/test-mcp", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ serverUrl })
